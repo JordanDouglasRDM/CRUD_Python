43c7a1812c7576b9aed3cfdceacea8cb94d6173e
Jordan Douglas <113628660+JordanDouglasRDM@users.noreply.github.com>
2023-02-05 21:34:39 -0300

    Add files via upload

---
+++ CRUD com Python.py
@@ -0,0 +1,42 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(#conexão com database
+    host ='localhost',
+    user ='root',
+    password ='',
+    database ='bdyoutube',
+)
+cursor = conexao.cursor() #executar os comandos da minha conexão, iniciar e fechar
+
+#CREATE
+nome_produto = "banana"
+valor = 7
+comando = f'INSERT INTO vendas (nome_produto, valor) ' \
+          f'VALUES ("{nome_produto}", "{valor}")' #observar a ordem de inserção no database (nome das colunas)
+cursor.execute(comando)
+conexao.commit()#edita o banco de dados
+resultado = cursor.fetchall() #ler banco de dados
+
+
+#READ
+comando = 'SELECT * FROM vendas'
+cursor.execute(comando)
+resultado = cursor.fetchall()
+print(resultado)
+
+#UPDATE
+valor = 5
+qualID = 2
+comando = f'UPDATE vendas SET valor = "{valor}" WHERE idVendas = {qualID}'
+cursor.execute(comando)
+conexao.commit()
+
+#DELETE
+qualID = 3
+comando = f'DELETE FROM vendas WHERE idVendas = {qualID}'
+cursor.execute(comando)
+conexao.commit()
+
+
+cursor.close()#fechar
+conexao.close()#fechar
